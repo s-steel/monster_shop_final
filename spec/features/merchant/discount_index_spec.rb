f896e4eb511bd032442e3debe876da6df9b9e239
Sean Steel <seanrsteel@gmail.com>
2020-11-07 14:45:39 -0700

    Add discount index page with info on each discount

--- spec/features/merchant/discount_index_spec.rb
+++ spec/features/merchant/discount_index_spec.rb
@@ -17,15 +17,32 @@ RSpec.describe 'Merchant Discount Index' do
       @order_item_2 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: true)
       @order_item_3 = @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: false)
       @order_item_4 = @order_3.order_items.create!(item: @giant, price: @giant.price, quantity: 4, fulfilled: false)
+
+      @discount_1 = @merchant_1.discounts.create!(discount: 5, number_of_items: 10)
+      @discount_2 = @merchant_1.discounts.create!(discount: 10, number_of_items: 20)
+      @discount_3 = @merchant_2.discounts.create!(discount: 3, number_of_items: 5)
+      @discount_4 = @merchant_2.discounts.create!(discount: 6, number_of_items: 15)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+      visit '/merchant/discounts'
     end
 
     it 'I can link to my merchant discounts from the merchant dashboard' do
       visit '/merchant'
 
       click_link 'My Discounts'
-
       expect(current_path).to eq('/merchant/discounts')
+    end
+
+    it 'show list of all discounts for that merchant with dicount and number_of_items' do
+      expect(page).to have_content("Your Discounts:")
+
+      within "#discount-#{@discount_1.id}" do
+        expect(page).to have_content("Discount: #{@discount_1.discount}%, on #{@discount_1.number_of_items}")
+      end
+
+      within "#discount-#{@discount_2.id}" do
+        expect(page).to have_content("Discount: #{@discount_2.discount}%, on #{@discount_2.number_of_items}")
+      end
     end
   end
 end
